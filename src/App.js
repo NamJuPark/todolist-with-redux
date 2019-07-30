e1e3d44b508484da635c8fdb191fec2821183f4f
NamjuPark <qkrska66@naver.com>
2019-07-30 18:04:01 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -56,9 +56,23 @@ class App extends Component {
     })
   }
 
+  handleRemove = id => {
+    const { todos } = this.state
+
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    })
+  }
+
   render() {
     const { input, todos } = this.state
-    const { handleChange, handleCreate, handleKeyPress, handleToggle } = this
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove
+    } = this
     return (
       <TodoListTemplate
         form={
@@ -70,7 +84,11 @@ class App extends Component {
           />
         }
       >
-        <TodoItemList todos={todos} onToggle={handleToggle} />
+        <TodoItemList
+          todos={todos}
+          onToggle={handleToggle}
+          onRemove={handleRemove}
+        />
       </TodoListTemplate>
     )
   }
